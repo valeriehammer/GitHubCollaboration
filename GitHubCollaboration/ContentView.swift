d23bc1156a92f24c3151d610fbeebea24172514f
Valerie Hammer <vkellogg@wisc.edu>
2025-05-01 20:23:26 -0500

    Add Change 4 to Content View

--- GitHubCollaboration/ContentView.swift
+++ GitHubCollaboration/ContentView.swift
@@ -17,6 +17,7 @@ struct ContentView: View {
             Text("Change 1")
             Text("Change 2")
             Text("Change 3 Ashley")
+            Text("Change 4 Val")
         }
         .padding()
     }
